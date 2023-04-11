be5bb09f9e41c9263efaaa5233b4f6bc98837489
Kieran Collins <kiecol@protonmail.com>
2023-04-11 19:44:25 +0000

    Set up database mmposts and comments and migrate to database

--- art_gallery/models.py
+++ art_gallery/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Private"), (1, "Public"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="image_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    post_image = CloudinaryField('image', default='placeholder')  # Placeholder to be removed...
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=1)  # This makes user uploads public by default
+    likes = models.ManyToManyField(User, related_name='image_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)  # Comments are visible by default
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
