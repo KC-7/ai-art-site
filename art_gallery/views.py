e0814163692b477fb92eb47ac8de8f7d2756de1b
Kieran Collins <kiecol@protonmail.com>
2023-04-12 16:16:15 +0000

    Messgaes & Account Templates: Set up messages to improve UX and set up remaing templates, Logout and Sign Up

--- art_gallery/views.py
+++ art_gallery/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
-from django.contrib import messages  # Review ......
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
